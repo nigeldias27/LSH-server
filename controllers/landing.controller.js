0374d6f435cfb4f60064209f0acb827d891f00ee
nigeldias27 <nigeldias27@yahoo.com>
2022-10-23 12:05:04 +0530

    Modified pending forms controller

--- controllers/landing.controller.js
+++ controllers/landing.controller.js
@@ -1,17 +1,13 @@
-import { Submission } from "../models/index.js";
+import { Submission, User } from "../models/index.js";
 
 export async function pendingForms(req, res) {
   var l = [];
   try {
-    const submissions = await Submission.find({});
-    for (let i = 0; i < submissions.length; i++) {
-      const submission = submissions[i];
-      for (let j = 0; j < submission.user.length; j++) {
-        const user = submission.user[j];
-        if (user == req.userid && submission.user.length != 4) {
-          l.push(submission);
-        }
-      }
+    const users = await User.findById(req.userid);
+    for (let i = 0; i < users.previousSubmisson.length; i++) {
+      const submissionId = users.previousSubmisson[i];
+      const submission = await Submission.findById(submissionId);
+      l.push(submission);
     }
     res.json(l);
   } catch (error) {
